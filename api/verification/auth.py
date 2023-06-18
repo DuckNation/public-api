750752c783bb0963f709f7a950b52b4b09dacdcf
haappi <74637670+haappi@users.noreply.github.com>
2023-06-18 01:51:22 -0500

    sighs

--- api/verification/auth.py
+++ api/verification/auth.py
@@ -7,15 +7,7 @@ from database import RedisSingleton
 
 
 async def verify_ws_api_key(websocket: WebSocket, redis=Depends(RedisSingleton.get_instance)):
-    api_key = None
-    if "key" in websocket.query_params:
-        api_key = websocket.query_params["key"]
-
-    if api_key is None:
-        if "Authorization" in websocket.headers:
-            header_value = websocket.headers["Authorization"]
-            if header_value.startswith("Bearer "):
-                api_key = header_value.split(" ")[-1]
+    api_key = await get_api_key(websocket)
 
     if api_key is None:
         raise WebSocketException(code=1008, reason="API key not found")
@@ -25,21 +17,26 @@ async def verify_ws_api_key(websocket: WebSocket, redis=Depends(RedisSingleton.g
     return api_key
 
 
+async def get_api_key(req: WebSocket | Request) -> str | None:
+    api_key = None
+    if "key" in req.query_params:
+        api_key = req.query_params["key"]
+
+    if api_key is None:
+        if "Authorization" in req.headers:
+            header_value = req.headers["Authorization"]
+            if header_value.startswith("Bearer "):
+                api_key = header_value.split(" ")[-1]
+
+    return api_key
+
 async def is_valid_api_key(api_key: str, redis):
     result = await redis.sismember("api-keys", api_key)
     return result
 
 
 async def verify_http_api_key(request: Request, redis=Depends(RedisSingleton.get_instance)):
-    api_key = None
-    if "key" in request.query_params:
-        api_key = request.query_params["key"]
-
-    if api_key is None:
-        if "Authorization" in request.headers:
-            header_value = request.headers["Authorization"]
-            if header_value.startswith("Bearer "):
-                api_key = header_value.split(" ")[-1]
+    api_key = await get_api_key(request)
 
     if api_key is None:
         raise HTTPException(status_code=403, detail="API key not found")
