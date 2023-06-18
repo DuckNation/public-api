053e99f6efcf90c9b0a0b88de79d8d3d9a32fa18
haappi <74637670+haappi@users.noreply.github.com>
2023-06-18 01:45:21 -0500

    someones going to die soon

--- api/__init__.py
+++ api/__init__.py
@@ -4,10 +4,10 @@ from api.verification import router as verification_router
 from api.wss import router as wss_router
 from utils.APIRouter import APIRouter
 from .verification import verify, create_pin, unverify
-from .verification.auth import verify_http_api_key
+from .verification.auth import verify_http_api_key, verify_ws_api_key
 
 router = APIRouter(prefix="/api", tags=["Protected API"])
 
 router.include_router(verification_router, dependencies=[Depends(verify_http_api_key)])
-router.include_router(wss_router)
+router.include_router(wss_router, dependencies=[Depends(verify_ws_api_key)])
 # router.include_router(info_router, dependencies=[Depends(verify_api_key)])
--- api/verification/auth.py
+++ api/verification/auth.py
@@ -1,4 +1,5 @@
 from fastapi import Depends, HTTPException
+from starlette.exceptions import WebSocketException
 from starlette.requests import Request
 from starlette.websockets import WebSocket
 
@@ -7,7 +8,6 @@ from database import RedisSingleton
 
 async def verify_ws_api_key(websocket: WebSocket, redis=Depends(RedisSingleton.get_instance)):
     api_key = None
-    print(websocket.query_params)
     if "key" in websocket.query_params:
         api_key = websocket.query_params["key"]
 
@@ -18,10 +18,9 @@ async def verify_ws_api_key(websocket: WebSocket, redis=Depends(RedisSingleton.g
                 api_key = header_value.split(" ")[-1]
 
     if api_key is None:
-        raise HTTPException(status_code=403, detail="API key not found")
-
+        raise WebSocketException(code=1008, reason="API key not found")
     if not await is_valid_api_key(api_key, redis):
-        raise HTTPException(status_code=403, detail="Invalid API key")
+        raise WebSocketException(code=1008, reason="Invalid API key")
 
     return api_key
 
@@ -33,7 +32,6 @@ async def is_valid_api_key(api_key: str, redis):
 
 async def verify_http_api_key(request: Request, redis=Depends(RedisSingleton.get_instance)):
     api_key = None
-    print(request.query_params)
     if "key" in request.query_params:
         api_key = request.query_params["key"]
 
