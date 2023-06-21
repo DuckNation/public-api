3121e7ac07cf73edfb7649b5de8463c8f4453709
haappi <74637670+haappi@users.noreply.github.com>
2023-06-20 21:30:08 -0500

    add a method to set discord channel id

---
+++ api/chats/set_discord.py
@@ -0,0 +1,23 @@
+from typing import Optional
+
+from fastapi import APIRouter, HTTPException
+
+from api.chats.Chat import Chat
+from database import MongoSingleton
+
+router = APIRouter()
+
+
+@router.put("/set-discord", status_code=200, response_model=Chat)
+async def discord_endpoint(chat_uuid: str, channel_id: Optional[int] = None):
+    instance = await MongoSingleton.get_instance()
+    exists = await instance.minecraft.chats.find_one({"_id": chat_uuid})
+    if not exists:
+        raise HTTPException(status_code=400, detail=f"Chat doesn't seem to exist.")
+
+    exists['discord_id'] = channel_id
+
+    chat = Chat(**exists)
+
+    await instance.minecraft.chats.replace_one({"_id": exists['_id']}, chat.dict(), upsert=True)
+    return chat
