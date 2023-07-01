ac8790ef5e05104e3d0e91284c5016e6c6e459ae
haappi <74637670+haappi@users.noreply.github.com>
2023-07-01 13:37:55 -0500

    exactly what youi think it is

--- api/chats/get_chats_for.py
+++ api/chats/get_chats_for.py
@@ -41,5 +41,31 @@ async def get_endpoint(
             chats.append(
                 {result["_id"]: result["discordId"] if "discordId" in result else None}
             )
+    print(chats)
 
     return chats
+
+
+@router.get("/better-get", status_code=200, description="Get a list of chats.")
+async def better_get_endpoint(
+        instance: pymongo.MongoClient = Depends(get_mongo_instance),
+        chat_uuid: Optional[str] = None,
+):
+    if chat_uuid:
+        try:
+            chat_uuid = format_uuid_args(chat_uuid)
+            return await instance.minecraft.chats.find_one({"_id": chat_uuid})
+        except ValueError:
+            return await instance.minecraft.chats.find_one(
+                {"name": chat_uuid.lower().replace(" ", "-")}
+            )
+    else:
+        chats = []
+        results = instance.minecraft.chats.find()
+        async for result in results:
+            chats.append({
+                "name": result["name"],
+                "uuid": result["_id"],
+                "discordId": result["discordId"] if "discordId" in result else None
+            })
+        return chats
